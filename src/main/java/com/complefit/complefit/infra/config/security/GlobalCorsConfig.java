9568a7e0054620976b71f88e35cac3c161ce1068
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2025-10-21 00:38:33 +0800

    Adding cors global config

---
+++ src/main/java/com/complefit/complefit/infra/config/security/GlobalCorsConfig.java
@@ -0,0 +1,31 @@
+package com.complefit.complefit.infra.config.security;
+
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:8083",    // Expo Web
+                "http://localhost:19006",   // Expo Go (emulator)
+                "https://complefit.com"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
